39dce382860df6ac452925e3c5daba4f7fab9435
Kevin Sy <kevsy@ucdavis.edu>
2023-11-11 21:39:07 -0800

    user store updated to have update user info and credentials and grab all users

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -1,6 +1,9 @@
 // constants
 const SET_USER = "session/SET_USER";
 const REMOVE_USER = "session/REMOVE_USER";
+const GET_ALL_USER = "session/GET_ALL_USER";
+const UPDATE_USER_INFO = "session/UPDATE_USER_INFO";
+const UPDATE_CREDENTIALS = "session/UPDATE_CREDENTIALS";
 
 const setUser = (user) => ({
 	type: SET_USER,
@@ -11,7 +14,87 @@ const removeUser = () => ({
 	type: REMOVE_USER,
 });
 
+const setAllUser = (user) => ({
+	type: GET_ALL_USER,
+	user
+});
+
+const updateUserInfoAction = (user) => ({
+	type: UPDATE_USER_INFO,
+	user
+});
+
+const updateCredentialAction = (user) => ({
+	type: UPDATE_CREDENTIALS,
+	user
+});
+
 const initialState = { user: null };
+
+export const fetchUsers = () => async (dispatch) => {
+	try{
+		const res = await fetch("/api/users");
+
+		if (res.ok) {
+			const data = await res.json();
+			dispatch(setAllUser(data));
+			return data;
+		} else {
+			const errors = await res.json();
+			return errors;
+		}
+	} catch (error) {
+		console.error("Error fetching users", error)
+		return error;
+	}
+}
+
+
+export const updateUserInfo = (updateInfoData) => async (dispatch) => {
+	try {
+		const res = await fetch("/api/auth/update/info", {
+			method: "PUT",
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify(updateInfoData)
+		});
+
+		if (res.ok) {
+			const updatedData = await res.json();
+			dispatch(updateUserInfoAction(updatedData))
+			return updatedData;
+		} else {
+			const errors = await res.json();
+			return errors;
+		}
+	} catch (error) {
+		console.error("Error updating user info", error)
+		return error;
+	}
+}
+
+
+export const updateCredential = (updateInfoData) => async (dispatch) => {
+	try {
+		const res = await fetch("/api/auth/update/credential", {
+			method: "PUT",
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify(updateInfoData)
+		});
+
+		if (res.ok) {
+			const updatedData = await res.json();
+			dispatch(updateCredentialAction(updatedData))
+			return updatedData;
+		} else {
+			const errors = await res.json();
+			return errors;
+		}
+	} catch (error) {
+		console.error("Error updating user credential", error)
+		return error;
+	}
+}
+
 
 export const authenticate = () => async (dispatch) => {
 	const response = await fetch("/api/auth/", {
@@ -97,11 +180,22 @@ export const signUp = (first_name, last_name, username, email, password) => asyn
 };
 
 export default function reducer(state = initialState, action) {
+	let newState = { ...state };
 	switch (action.type) {
 		case SET_USER:
 			return { user: action.payload };
 		case REMOVE_USER:
 			return { user: null };
+		case GET_ALL_USER:
+			const allUser = action.users;
+			newState.user = allUser;
+			return newState;
+		case UPDATE_USER_INFO:
+			newState[action.user.id] = action.user;
+			return newState;
+		case UPDATE_CREDENTIALS:
+			newState[action.user.id] = action.user;
+			return newState;
 		default:
 			return state;
 	}
