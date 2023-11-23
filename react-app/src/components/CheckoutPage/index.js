d0aa961bfbd60278363105338c38827f25a21b82
Kevin Sy <kevsy@ucdavis.edu>
2023-11-22 22:31:05 -0800

    temporaryily added address and payment to checkout page

--- react-app/src/components/CheckoutPage/index.js
+++ react-app/src/components/CheckoutPage/index.js
@@ -8,9 +8,9 @@ import React, { useEffect, useState } from 'react';
 
 
 function CheckoutPage() {
-
     const dispatch = useDispatch();
     const cartItems = useSelector((state) => state.carts.carts)
+    const sessionUser = useSelector(state => state.session.user);
     const addressData = useSelector((state) => state.addresses.addresses)
     const paymentData = useSelector((state) => state.payments.payments)
 
@@ -18,10 +18,34 @@ function CheckoutPage() {
     const allAddress = addressData ? Object.values(addressData.addresses) : []
     const allPayment = paymentData ? Object.values(paymentData.payments) : []
 
+    const [ changeAddress, setChangeAddress ] = useState(false)
+    const [ city, setCity ] = useState("");
+    const [ address, setAddress ] = useState("");
+    const [ state, setState ] = useState("");
+    const [ zip, setZip ] = useState("");
+    const [ selectedAddress, setSelectedAddress ] = useState(false)
+
     const [changePayment, setChangePayment] = useState(false)
     const [selectedPaymentCard, setSelectedPaymentCard] = useState("")
+    const [paymentSelected, setPaymentSelected] = useState(false)
     const [selectedPaymentAddress, setSelectedPaymentAddress] = useState("")
     const card_name = ["American Express", "Visa", "Mastercard", "Discover"];
+
+    if (city === "" && selectedAddress === false) {
+        allAddress.map((address) => {
+            if(address?.primary) {
+                setCity(address?.city)
+                setAddress(address?.address)
+                setState(address?.state)
+                setZip(address?.zip)
+            } else if (address?.id === 1) {
+                setCity(address?.city)
+                setAddress(address?.address)
+                setState(address?.state)
+                setZip(address?.zip)
+            }
+        })
+    }
 
     function findCardName (number) {
         if (number === "3" || number === 3) {
@@ -55,8 +79,12 @@ function CheckoutPage() {
         setChangePayment(!changePayment)
     }
 
+    function changeAddressButtonDropDown () {
+        setChangeAddress(!changeAddress)
+    }
+
     //set primary payment on render
-    if(selectedPaymentCard === ""){
+    if(selectedPaymentCard === "" && paymentSelected === false){
         allPayment.map((payment) => {
             if(payment?.primary) {
                 setSelectedPaymentCard(payment?.card_number)
@@ -66,11 +94,6 @@ function CheckoutPage() {
                 setSelectedPaymentAddress(payment?.billing_address)
             }
         });
-    }
-
-    //switch payment method function
-    function changePaymentMethod (id) {
-
     }
 
     useEffect(() => {
@@ -86,39 +109,86 @@ function CheckoutPage() {
                     <div className='address-checkout-container'>
                         <div className='address-checkout-wrapper'>
                             <div className='address-checkout-content'>
+                                <div className='address-info-checkout-content'>
+                                    <div className='address-info-left'>
+                                        <h3>Shipping address</h3>
+                                    </div>
+                                    <div className='address-info-middle'>
+                                        <div className='address-info'>{sessionUser?.first_name} {sessionUser?.middle_name || ""} {sessionUser?.last_name}</div>
+                                        <div className='address-info'>{address}</div>
+                                        <div className='address-info'>{city}, {state} {zip}</div>
+                                    </div>
+                                    <div className='address-info-right'>
+                                        <button onClick={changeAddressButtonDropDown}>Change</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='payment-checkout-content-dropdown'>
+                                <div className={`payment-info-dropdown-container ${changeAddress ? 'active' : 'inactive'}`}>
+                                    Select Address
+                                </div>
+                                {allAddress.map((address, index) => {
+                                    function changeAddressMethod() {
+                                        setCity(address?.city)
+                                        setAddress(address?.address)
+                                        setState(address?.state)
+                                        setZip(address?.zip)
+                                        setSelectedAddress(true)
+                                        setChangeAddress(false)
+                                    }
+                                    if(address && changeAddress) {
+                                        return (
+                                            <div key={index} onClick={changeAddressMethod} className='address-info-container'>
+                                                <div className='address-info'>{sessionUser?.first_name} {sessionUser?.last_name}</div>
+                                                <div className='address-info'>{address?.address}</div>
+                                                <div className='address-info'>{address?.city}, {address?.state} {address?.zip}</div>
+                                                <div className='address-info'>{address?.country}</div>
+                                            </div>
+                                        )
+                                    }
+                                })}
                             </div>
                         </div>
                     </div>
                     <div className='payment-checkout-container'>
                         <div className='payment-checkout-wrapper'>
                             <div className='payment-checkout-content'>
-                                <div className='address-info-checkout-content'>
-                                    <div className='address-info-left'>
+                                <div className='payment-info-checkout-content'>
+                                    <div className='payment-info-left'>
                                         <h3>Payment method</h3>
                                     </div>
-                                    <div className='address-info-middle'>
-                                        <div className='address-info'>{findCardIcon(selectedPaymentCard.toString().slice(0,1))} Paying
+                                    <div className='payment-info-middle'>
+                                        <div className='payment-info'>{findCardIcon(selectedPaymentCard.toString().slice(0,1))} Paying
                                             with {findCardName(selectedPaymentCard.toString().slice(0,1))} {selectedPaymentCard.toString().slice(-4)}
                                         </div>
-                                        <div className='address-info'>Billing address: {selectedPaymentAddress}</div>
+                                        <div className='payment-info'>Billing address: {selectedPaymentAddress}</div>
                                     </div>
-                                    <div className='address-info-right'>
+                                    <div className='payment-info-right'>
                                         <button onClick={changePaymentButtonDropDown}>Change</button>
                                     </div>
                                 </div>
                             </div>
                             <div className='payment-checkout-content-dropdown'>
-                                <div className={`address-info-dropdown-container ${changePayment ? 'active' : 'inactive'}`}>
+                                <div className={`payment-info-dropdown-container ${changePayment ? 'active' : 'inactive'}`}>
                                     Select Payment
                                 </div>
                                 {allPayment.map((payment, index) => {
+
+                                    //change payment method
+                                    function changePaymentMethod() {
+                                        setSelectedPaymentCard(payment?.card_number)
+                                        setSelectedPaymentAddress(payment?.billing_address)
+                                        setPaymentSelected(true)
+                                        setChangePayment(false)
+                                    }
+
                                     if(payment && changePayment) {
                                         return (
-                                            <div key={index} className='address-info-dropdown-content'>
-                                                <div className='address-dropdown-info'>{findCardIcon(payment?.card_number.toString().slice(0,1))}</div>
-                                                <div className='address-dropdown-info'>{payment?.name}</div>
-                                                <div className='address-dropdown-info'>{findCardName(payment?.card_number.toString().slice(0,1))}</div>
-                                                <div className='address-dropdown-info'>Credit card ending in {payment?.card_number.toString().slice(-4)}</div>
+                                            <div key={index}  onClick={changePaymentMethod} className='payment-info-dropdown-content'>
+                                                <div className='payment-dropdown-info'>{findCardIcon(payment?.card_number.toString().slice(0,1))}</div>
+                                                <div className='payment-dropdown-info'>{payment?.name}</div>
+                                                <div className='payment-dropdown-info'>{findCardName(payment?.card_number.toString().slice(0,1))}</div>
+                                                <div className='payment-dropdown-info'>Credit card ending in {payment?.card_number.toString().slice(-4)}</div>
                                             </div>
                                         )
                                     }
